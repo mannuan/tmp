e89d85bfdf575d266b8df67c19a8405ed06eb22a
mannuan <1271990125@qq.com>
2017-11-25 10:57:11 +0800

    phantomjs

--- tmp/untitled0.py
+++ tmp/untitled0.py
@@ -6,13 +6,14 @@ Created on Thu Nov 23 08:36:17 2017
 @author: mininet
 """
 
-import os,requests
-file_url = 'http://www.h2o-china.com/law/view/download?id=1'
-file_id = file_url.split('=')[-1]
-file_name = '{}/zhongguoshuiwang_paper/{}.pdf'.format(os.environ['HOME'],file_id)
-r = requests.get(file_url, stream=True)
-f = open(file_name, "wb+")
-for chunk in r.iter_content(chunk_size=512):
-    if chunk:
-        f.write(chunk)
-f.close()
+import os,requests,pymysql
+conn = pymysql.connect(host='122.224.129.35', port=23306, user='repository', passwd='repository', db='repository',charset='utf8')
+cur = conn.cursor()
+cur.execute("select * from testType where name = '河长制政策'")
+rows = cur.fetchall()
+print rows[0][2]
+# 释放数据连接
+if cur:
+    cur.close()
+if conn:
+    conn.close()
